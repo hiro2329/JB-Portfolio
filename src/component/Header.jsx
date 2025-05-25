34ddc706af54a1963a10440f1da6afbb6282f29a
hiro2329 <hiro2329@naver.com>
2025-05-25 15:04:49 +0900

    모바일 드롭다운 클릭 스크롤 애니메이션 구현완료

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -13,14 +13,16 @@ const Header = ({ heroRef, introduceRef, projectRef }) => {
         <div>
             <header className="fixed inset-x-0 top-0 z-50" >
                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-                    <ul className="flex lg:flex-1 ">
-                        <li
-                            className="cursor-pointer text-sm/6 font-semibold text-gray-900 bg-transparent transition hover:text-blue-600"
-                            onClick={() => scrollToSection(heroRef)}
-                        >
-                            Home
-                        </li>
-                    </ul>
+                    <div className="flex flex-1">
+                        <ul className="hidden lg:flex ">
+                            <li
+                                className="cursor-pointer text-sm/6 font-semibold text-gray-900 bg-transparent transition hover:text-blue-600"
+                                onClick={() => scrollToSection(heroRef)}
+                            >
+                                Home
+                            </li>
+                        </ul>
+                    </div>
                     <div className="flex lg:hidden">
                         <button
                             type="button"
@@ -59,7 +61,6 @@ const Header = ({ heroRef, introduceRef, projectRef }) => {
                         <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                             <div className="flex items-center justify-between">
                                 <a href="#" className="-m-1.5 p-1.5">
-                                    <span className="sr-only">Your Company</span>
                                     <img className="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="" />
                                 </a>
                                 <button
@@ -76,10 +77,44 @@ const Header = ({ heroRef, introduceRef, projectRef }) => {
                             <div className="mt-6 flow-root">
                                 <div className="-my-6 divide-y divide-gray-500/10">
                                     <div className="space-y-2 py-6">
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">About</a>
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Skills</a>
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Project</a>
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Experience</a>
+                                        <button
+                                            className="-mx-3 block w-full text-left rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                                            onClick={() => {
+                                                scrollToSection(heroRef);
+                                                setMobileMenuOpen(false);
+                                            }}
+                                        >
+                                            Home
+                                        </button>
+                                        <button
+                                            className="-mx-3 block w-full text-left rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                                            onClick={() => {
+                                                scrollToSection(introduceRef);
+                                                setMobileMenuOpen(false);
+                                            }}
+                                        >
+                                            Introduce
+                                        </button>
+                                        <button
+                                            className="-mx-3 block w-full text-left rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                                            onClick={() => {
+                                                scrollToSection(projectRef);
+                                                setMobileMenuOpen(false);
+                                            }}
+                                        >
+                                            Projects
+                                        </button>
+
+                                        <button
+                                            className="-mx-3 block w-full text-left rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                                            onClick={() => {
+                                                scrollToSection(projectRef);
+                                                setMobileMenuOpen(false);
+                                            }}
+                                        >
+                                            Contact
+                                        </button>
+
                                     </div>
                                 </div>
                             </div>
@@ -87,7 +122,7 @@ const Header = ({ heroRef, introduceRef, projectRef }) => {
                     </div>
                 )}
             </header>
-        </div>
+        </div >
     );
 }
 export default Header;
